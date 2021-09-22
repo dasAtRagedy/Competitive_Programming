2a3ad221b4b0b8bb1938625203264e1eda0375a3
David <taitragedija@gmail.com>
2021-09-22 15:09:49 +0300

    Create deletron.cpp
    Made a program and added it to auto-start just to delete .o and .exe files when windows start

---
+++ deletron.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    system("del /Q \"C:\\Users\\Dunix\\Desktop\\Competitive_Programming\\CSIS Problemset\\*.o\"");
+    system("del /Q \"C:\\Users\\Dunix\\Desktop\\Competitive_Programming\\CodeForces Problemset\\*.o\"");
+    system("del /Q \"C:\\Users\\Dunix\\Desktop\\Competitive_Programming\\CSIS Problemset\\*.exe\"");
+    system("del /Q \"C:\\Users\\Dunix\\Desktop\\Competitive_Programming\\CodeForces Problemset\\*.exe\"");
+    return 0;
+}
