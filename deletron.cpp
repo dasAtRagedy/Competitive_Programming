00173462e240c42aba229b3b2ca7582ceed7fee4
David <taitragedija@gmail.com>
2021-09-23 14:20:28 +0300

    Create deletron.cpp

---
+++ deletron.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    system("del /Q \"CSES Problemset\\*.o\"");
+    system("del /Q \"CodeForces Problemset\\*.o\"");
+    system("del /Q \"CSES Problemset\\*.exe\"");
+    system("del /Q \"CodeForces Problemset\\*.exe\"");
+    return 0;
+}
